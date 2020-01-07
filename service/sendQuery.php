0fd15f5db5fdaf6c04fb279a318c5fb41859d2ae
Maverick1302 <38353652+sudipto1304@users.noreply.github.com>
2020-01-07 14:09:22 -0600

    asdas

--- service/sendQuery.php
+++ service/sendQuery.php
@@ -5,12 +5,18 @@ $email = urlencode($_POST["email"]);
 $contact = urlencode($_POST["mobile"]);
 $message = urlencode($_POST["message"]);
 
+
+
 $sendMail = new Email();
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "Invalid Email Address";
     die();
 }else{
+    if(empty($name) || empty($email) || empty($contact) || empty($message)){
+        echo "All fields are mandatory";
+        die();
+    }
     $sendMail->send($name, $email, $contact, $message);
     
 }
