06206b5d3ccbd70b2de16ff9f961c6af8c451278
Maverick1302 <38353652+sudipto1304@users.noreply.github.com>
2020-01-26 00:51:38 -0600

    asdsad

--- service/DriveApiService.php
+++ service/DriveApiService.php
@@ -1,17 +1,25 @@
 <?php
-
+require_once($_SERVER['DOCUMENT_ROOT']."/service/constant.php"); 
 class DriveApiManager{
     
+    static $accessToken = "";
+    
+    
     public function getContent(){
-        $this->getAccessToken();
+        //$this->getAccessToken();
+        
+        $_url = "https://www.googleapis.com/drive/v2/files/"."0B_ioEFehPeW5amVGYXYxUlpZbVU"."/children?access_token=".$accessToken;
+        $jsonStrig = file_get_contents($_url);
+        $json = json_decode(preg_replace('/("\w+"):(\d+)/', '\\1:"\\2"', $jsonStrig), true);
+        var_dump($json);
     }
     
     
     private function getAccessToken(){
         $postdata = http_build_query(array(
-                    'client_id' => '30181947658-uc1fpdhbg38p8i4qmakq35tnfbma0hnk.apps.googleusercontent.com',
-                    'client_secret' => 'VfftJJFj8fvKBbAYjQn7u3TL',
-                    'refresh_token' => '1//0fl-jCsn1POJlCgYIARAAGA8SNwF-L9IrSwhWQjhWxUrmS2viVTtQQnWtSGqaDhXxQhXdmyRbhiZifmL6Xg44vji38WpChmIp7Sc',
+                    'client_id' => CLINET_ID,
+                    'client_secret' => CLIENT_SECRET,
+                    'refresh_token' => REFRESH_TOKEN,
                     'grant_type' => 'refresh_token',
                 )
             );
@@ -26,7 +34,7 @@ class DriveApiManager{
             
         $jsonStrig = file_get_contents('https://www.googleapis.com/oauth2/v4/token', false, $context);
         $json = json_decode(preg_replace('/("\w+"):(\d+)/', '\\1:"\\2"', $jsonStrig), true);
-        var_dump($json);
+        $accessToken =  $json["access_token"];
     }
     
 }
