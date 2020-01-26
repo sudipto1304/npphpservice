ac96746a0f40a960bf13170c6d71dd4d131a9f86
Maverick1302 <38353652+sudipto1304@users.noreply.github.com>
2020-01-26 01:03:59 -0600

    Update DriveApiService.php

--- service/DriveApiService.php
+++ service/DriveApiService.php
@@ -14,7 +14,13 @@ class DriveApiManager{
                         )
                     );
         $context  = stream_context_create($opts);
-        $jsonStrig = file_get_contents($_url, false, $context);
+        $jsonStrig = @file_get_contents($_url, false, $context);
+        if(!$jsonStrig){
+            $this->getAccessToken();
+            $_url = "https://www.googleapis.com/drive/v2/files/"."0B_ioEFehPeW5amVGYXYxUlpZbVU"."/children?access_token=".$accessToken;
+            $jsonStrig = @file_get_contents($_url, false, $context);
+        }
+        
         $json = json_decode(preg_replace('/("\w+"):(\d+)/', '\\1:"\\2"', $jsonStrig), true);
         var_dump($json);
     }
